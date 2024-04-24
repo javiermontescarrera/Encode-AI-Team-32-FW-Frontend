63f1a0c9e66e5130f13bdbefe9d0b6e45cafc3e4
Javier Montes Carrera <javier.montes.carrera@gmail.com>
2024-04-24 18:40:35 +0000

    feat: Show diagnose details which were saved in the blockchain

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -11,8 +11,6 @@ import { useEffect, useState } from "react";
 // const backend_url = `${window.location.protocol}//${window.location.hostname}:${process.env.NEXT_PUBLIC_BACKEND_PORT}`;
 const backend_url = `http://${window.location.hostname}:${process.env.NEXT_PUBLIC_BACKEND_PORT}`;
 
-// let selectedHash: string;
-
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
   const [contractAddress, setContractAddress] = useState();
@@ -42,7 +40,7 @@ const Home: NextPage = () => {
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           {
-            (connectedAddress) ? <ContractAddress /> : <div></div>
+            (connectedAddress) ? <Diagnoses /> : <div></div>
           }
         </div>
       </div>
@@ -51,16 +49,18 @@ const Home: NextPage = () => {
 };
 
 
-function ContractAddress() {
+function Diagnoses() {
   const { address: connectedAddress } = useAccount();
 
   const [data, setData] = useState<{ result: string[] }>();
   const [isLoading, setLoading] = useState(true);
   const [selectedHash, setSelectedHash] = useState("");
-
+  const [diagnoseDetailsData, setDiagnoseDetailsData] = useState<{ result: object }>();
+  const [isdiagnoseDetailsLoading, setDiagnoseDetailsLoading] = useState(true);
+  
   function handleHashChange(e:any) {
     setSelectedHash(e.currentTarget.value);
-    // console.log(`selectedHash: ${selectedHash}`);
+    // console.log(`selectedHash: ${e.currentTarget.value}`);
   }
 
   useEffect(() => {
@@ -98,21 +98,76 @@ function ContractAddress() {
         </p>
       </div>
       <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-        <p>
-          {
-            (selectedHash !== "") ? 
-              <div>
-                <div className="flex justify-center items-center space-x-2">
-                  <p className="my-2 font-medium">{selectedHash?.slice(0, 6) + "..." + selectedHash?.slice(-4)}</p>
-                </div>
-                <img src={"https://ipfs.io/ipfs/" + selectedHash} alt="" /> 
-              </div>
-            : <></>
-
-          }
-        </p>
+        <DiagnoseDetails selectedHash={selectedHash}/>
       </div>
     </div>
+  );
+}
+
+function DiagnoseDetails(params: any) {
+  const [data, setData] = useState<{ result: string[] }>();
+  const [isLoading, setLoading] = useState(true);
+  // const [objBody, setBody] = useState({ diagnoseHash: "" });
+
+  const trimmedHash = params.selectedHash?.slice(0, 6) + "..." + params.selectedHash?.slice(-4);
+  // console.log(`trimmedHash: ${trimmedHash}`);
+
+  function formatTimestamp(timestamp:string) {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+    return new Date(Number(timestamp) * 1000).toLocaleDateString('en-US', options);
+  }
+  
+  useEffect(() => {
+    const objBody = { diagnoseHash: params.selectedHash };
+    // console.log(`objBody: ${JSON.stringify(objBody)}`);
+
+    fetch(backend_url + "/get-diagnose-details", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(objBody),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(`body: ${JSON.stringify(objBody)}`);
+        setData(data);
+        setLoading(false);
+        // console.log(data.result);
+      });
+  }, [params.selectedHash]);
+
+  if (isLoading) return <p>Loading ${trimmedHash} diagnose details...</p>;
+  if (!data) return <p>No details found</p>;
+
+  return (
+    <p>
+      <div>
+        {  
+          (params.selectedHash !== "") ? 
+          <>
+            <div className="flex justify-center items-center space-x-2">
+              <p className="my-2 font-medium">{trimmedHash}</p>
+            </div>
+            <img src={"https://ipfs.io/ipfs/" + params.selectedHash} alt="" />
+          </>
+          : <></>
+        }
+        {
+          ((data.result) && params.selectedHash !== "") ? 
+            <span>
+              <p className="my-2 font-medium">Diagnose: {data.result[0]}</p>
+              <p className="my-2 font-medium">Registered on {new Date(Number(data.result[1]) * 1000).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })}</p>
+            </span>
+          : <></>
+        }
+      </div>
+    </p>
   );
 }
 
