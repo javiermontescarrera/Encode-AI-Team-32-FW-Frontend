8c04d2ea802a92c23029d7d2cf716b778b693c8b
Javier Montes Carrera <javier.montes.carrera@gmail.com>
2024-04-24 02:11:18 +0000

    feat: Getting contract address and listing patient hashes

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
+import { useTheme } from "next-themes";
 
 type HeaderMenuLink = {
   label: string;
@@ -58,6 +59,8 @@ export const HeaderMenuLinks = () => {
 export const Header = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const burgerMenuRef = useRef<HTMLDivElement>(null);
+  const { resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === "dark";
   useOutsideClick(
     burgerMenuRef,
     useCallback(() => setIsDrawerOpen(false), []),
@@ -90,16 +93,21 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            {
+              !isDarkMode ? 
+              <Image alt="logo" className="cursor-pointer" fill src="/logo.jpeg" />
+              :
+              <Image alt="logo" className="cursor-pointer" fill src="/logo_dark_theme.jpeg" />
+            }
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">AI Diagnose</span>
+            {/* <span className="text-xs">Ethereum dev stack</span> */}
           </div>
         </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
+        {/* <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
           <HeaderMenuLinks />
-        </ul>
+        </ul> */}
       </div>
       <div className="navbar-end flex-grow mr-4">
         <RainbowKitCustomConnectButton />
